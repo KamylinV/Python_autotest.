ec22231f60b5eb0d1fbd1be669f95e6dcf75845d
Victor Kamylin <victorkamylin@gmail.com>
2025-11-24 15:15:19 +0300

    Les

--- Study_autotest/Lesson_1.py
+++ Study_autotest/Lesson_1.py
@@ -1,3 +1,15 @@
-d = int(input())
+a = "1"
+print(type(a))
+a = int(a)
+print(type(a))
+f = []
+g = list()
+f.append(34)
+n = f.index(34)
+print(n)
 
-print(tggggg)
+cor = (1,2,4543,True,'name')
+print(cor.count('name'), cor.index('name'))
+w = 8
+
+ 
