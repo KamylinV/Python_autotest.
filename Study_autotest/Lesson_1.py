f9f702749a1b367537375f10903b39cbdbd9f0a6
Victor Kamylin <victorkamylin@gmail.com>
2025-11-26 19:41:00 +0300

    Stady

--- Study_autotest/Lesson_1.py
+++ Study_autotest/Lesson_1.py
@@ -1,15 +1,36 @@
-a = "1"
-print(type(a))
-a = int(a)
-print(type(a))
-f = []
-g = list()
-f.append(34)
-n = f.index(34)
-print(n)
 
-cor = (1,2,4543,True,'name')
-print(cor.count('name'), cor.index('name'))
-w = 8
+# распаковка можно делать когла мы знаем количество элементов в коллекции например
+# my_list = [2, 4]
+# a, s= my_list
+# print(a)
+# print(s)
+# my_tuple= ('sos',3,4)
+# c, d, e = my_tuple
+# print(c)
+# print(d)
+# print(e)
+# #срез
+# list1 = ['EDEFD', 4, 6, 'refre', 4, 5, 6]
+# print(list1[:4])
+#
+#
+# text= 'Hello my friend myself'
+#
+# print(text.index('fri'))
+# print(text.count('my'))
+# print('y' in text)
 
- 
+# my_string = 'Привет как дела или как не дела'
+# list2 = my_string.split(' ')
+# print(list2)
+# str_csv = 'Привет, как, дела, или, как, не, дела'
+# print(str_csv.split(','))
+# e = str_csv.split(',')
+# # превратить коллекцию в строку
+# srt1 =  ', '.join(e)
+# print(srt1)
+
+a = 'one'
+b = 'two'
+print('First world is', a, 'second world is',b )
+print(f'First world is {a}, second world is {b}' )
